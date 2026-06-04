11269618bb7e0a8b7d3ebe1d5fe851e2fdfaf95c
Navya Manoj P <navyamanojp123@gmail.com>
2026-06-04 15:56:23 +0530

    .

--- aiagents/day1/basic_ai_agents.py
+++ aiagents/day1/basic_ai_agents.py
@@ -1,13 +1,35 @@
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.prompts import PromptTemplate
 from langchain_ollama import OllamaLLM
 
 #Load AI Model from Ollama
 llm = OllamaLLM(model="mistral")
+#initialize Memory
+chat_history = ChatMessageHistory() #stores history
+#define ai prompt
+prompt=PromptTemplate(
+    input_variables=["chat_history","question"],
+    template="Previous conversation : {chat_history}\n User : {question}\n ai"
+)
 
-print("\n Welcome to your ai agent ! ask me anything")
+#function to run AI chat with memory
+def run_chain(question):
+    chat_history_text = "\n".join([f"{msg.type.capitalize()}: {msg.content}" for msg in chat_history.messages])
+    # Run the AI response generation
+    response = llm.invoke(prompt.format(chat_history=chat_history_text, question=question))
+    # Store new user input and AI response in memory
+    chat_history.add_user_message(question)
+    chat_history.add_ai_message(response)
+
+    return response
+
+print("\n Ai Chatbot with Memory")
+print("type 'exit' to stop ")
+
 while True:
-    question=input("Your Question (or type 'exit' to stop):")
-    if question.lower() == 'exit' :
+    user_input=input("\n You :")
+    if user_input().lower() == 'exit' :
         print("goodbye")
         break
-    response = llm.invoke(question)
-    print("\n ai response : ",response)
+    ai_response = run_chain(user_input)
+    print("\n AI : ",ai_response)
